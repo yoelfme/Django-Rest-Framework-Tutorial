be4e5616b8ff4761951c0a33aff9523889015d19
Yoel Monzon <yoelfme@hotmail.com>
2015-08-12 16:07:16 -0600

    [update] authentication API

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,7 @@
 from snippets.models import Snippet
+from django.contrib.auth.models import User
 from snippets.serializers import SnippetSerializer
+from snippets.serializers import
 from rest_framework import generics
 
 
@@ -7,7 +9,20 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perfom_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
