1005c42820830887df31afad26ef4b706516d907
Yoel Monzon <yoelfme@hotmail.com>
2015-09-16 19:34:04 -0600

    [fix] min, add import

--- snippets/views.py
+++ snippets/views.py
@@ -7,6 +7,7 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import renderers
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route
 
 class SnippetViewSet(viewsets.ModelViewSet):
     """
